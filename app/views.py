09bff9d12f0588d5daf279bfcc1ce9325f5d78c1
Rafael Casale Abe <rafael@movepass.com.br>
2015-04-09 23:31:26 -0300

    Second article of The Flask Mega-Tutorial. In this article we could link a python code to a template, control stament in templates and inherit a template from a base one

--- app/views.py
+++ app/views.py
@@ -1,7 +1,32 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-	return "Hello world!"
+	user_name = {'nickname': 'Casebre'} 
+	posts = [
+		{
+			'author': {'nickname': 'Jao'},
+			'body': 'Beatiful day in Portland'
+		},
+		{
+			'author': {'nickname': 'Susan'},
+			'body': 'The Avengers movie was so cool'
+		}
+	]
+	return render_template(
+			  'index.html',
+			  title = 'Main',
+			  user=user_name,
+			  posts=posts
+			)
+
+@app.route('/about')
+
+def about():
+	return render_template(
+			'about.html',
+			title = 'About'
+			)
